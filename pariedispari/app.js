27a3f34125a08af100512caad8edd88dea5be2fe
nicola00 <nicolajobs00@gmail.com>
2023-02-08 17:17:22 +0100

    FEAT if complete

--- pariedispari/app.js
+++ pariedispari/app.js
@@ -1,5 +1,3 @@
-console.log('pari')
-
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 // Sommiamo i due numeri
@@ -11,6 +9,7 @@ let pariDispariUser = prompt('scegli tra pari e dispari')
 let numUser = parseInt(prompt('segli un numero da 1 a 5'))
 
 
+
 let numPc = Math.floor(Math.random() * (5 - 1 + 1) + 1);
 
 let sommaPcUser = numPc + numUser
@@ -18,8 +17,17 @@ let sommaPcUser = numPc + numUser
 let restoSomma = sommaPcUser % 2
 
 if (restoSomma === 0){
-    console.log(sommaPcUser, restoSomma, 'il numero è pari')
+    console.log(numUser, numPc, sommaPcUser, 'il numero è pari')
 }else if (restoSomma === 1){
-    console.log(sommaPcUser, restoSomma, 'il numero è dispari')
+    console.log(numUser, numPc, sommaPcUser, 'il numero è dispari')
 }
 
+if ((restoSomma === 0) && (pariDispariUser === 'pari')){
+    console.log('hai vinto tu!!')
+} else if ((restoSomma === 1) && (pariDispariUser === 'dispari')){
+    console.log('hai vinto')
+} else {
+    console.log('hai perso')
+}
+
+console.log(pariDispariUser, sommaPcUser)

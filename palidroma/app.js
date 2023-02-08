f8c013050f682deb53644ccf068c95190d5cb282
nicola00 <nicolajobs00@gmail.com>
2023-02-08 15:07:46 +0100

    START

---
+++ palidroma/app.js
@@ -0,0 +1,6 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Comunicare all’utente (decidete voi come) se la parola che ha inserito è palindroma oppure no
+
+
+let parolaUtente = prompt('inserisci un parola palidroma')

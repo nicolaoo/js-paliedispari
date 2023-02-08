69e7803d14cd547cc6e312347a487c561a2b8b89
nicola00 <nicolajobs00@gmail.com>
2023-02-08 17:00:07 +0100

    FEAT chiedo pari o dispari e il numero

--- pariedispari/app.js
+++ pariedispari/app.js
@@ -1,1 +1,25 @@
 console.log('pari')
+
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Comunichiamo all’utente chi ha vinto. (decidete voi come)  
+
+
+let pariDispariUser = prompt('scegli tra pari e dispari') 
+let numUser = parseInt(prompt('segli un numero da 1 a 5'))
+
+
+let numPc = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+
+let sommaPcUser = numPc + numUser
+
+let restoSomma = sommaPcUser % 2
+
+if (restoSomma === 0){
+    console.log(sommaPcUser, restoSomma, 'il numero è pari')
+}else if (restoSomma === 1){
+    console.log(sommaPcUser, restoSomma, 'il numero è dispari')
+}
+
